794e61e8549df1ae77860c24223bfaea79db8c7f
Viktoria Gornostaeva <vv.gornostaeva@mail.ru>
2022-10-31 17:48:24 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, any_slug):
+    return HttpResponse(f'Страница с постами {any_slug}')
